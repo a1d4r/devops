abbf70dc42790cdc234f053117668552320b59f2
a1d4r <40607719+a1d4r@users.noreply.github.com>
2021-09-13 12:37:10 +0300

    Metrics (#8)
    * Add metrics for web app
    
    * Add monitoring
    
    * Add report for metrics

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -3,8 +3,10 @@ from zoneinfo import ZoneInfo
 
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 
 @app.get('/', response_class=PlainTextResponse)
