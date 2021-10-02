4714d3c0a863dac01d6e3f9875a93aa4297a6b86
a1d4r <40607719+a1d4r@users.noreply.github.com>
2021-10-02 21:15:32 +0300

    Add setting for storage file path (#14)

--- app_python/app/settings.py
+++ app_python/app/settings.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from zoneinfo import ZoneInfo
 
 from pydantic import BaseSettings
@@ -6,6 +7,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     datetime_format: str = '%H:%M:%S'
     timezone: ZoneInfo = ZoneInfo('Europe/Moscow')
+    filepath: Path = Path('visits.json')
 
 
 settings = Settings()
